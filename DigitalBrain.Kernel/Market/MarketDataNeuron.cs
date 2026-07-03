a126346956fd41bd92c2008b2fe3ba39927d6aa8
Vladyslav Horbachov <0965142838gvs@gmail.com>
2026-07-03 10:12:07 +0200

    feat(market): add MarketDataNeuron reacting to CheckBitcoinPrice

---
+++ DigitalBrain.Kernel/Market/MarketDataNeuron.cs
@@ -0,0 +1,19 @@
+using DigitalBrain.Core;
+
+namespace DigitalBrain.Kernel.Market;
+
+public interface IMarketDataNeuron : INeuron, IHandle<Signal> { }
+
+[GrainType("digitalbrain.market-data")]
+public class MarketDataNeuron(ILogger<MarketDataNeuron> logger, NeuronJournals journals, IMarketDataApiClient client)
+    : Neuron(logger, journals), IMarketDataNeuron
+{
+    public async Task HandleAsync(Signal signal)
+    {
+        if (signal.Name != "CheckBitcoinPrice") return;
+
+        var price = await client.GetBitcoinPriceUsdAsync();
+        var props = new Dictionary<string, object?>(signal.Props) { ["price"] = price };
+        await Broadcast(new Signal("BitcoinPriceChecked", props));
+    }
+}
