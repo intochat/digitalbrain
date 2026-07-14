eb843f19a0bb751bac3a8cd05727f58f7a56aa72
Vladyslav Horbachov <0965142838gvs@gmail.com>
2026-07-14 19:35:25 +0200

    test(flutter): align Forui harnesses with 0.24

--- app/test/ui_kit/ui_inputs_a_test.dart
+++ app/test/ui_kit/ui_inputs_a_test.dart
@@ -39,7 +39,7 @@ void main() {
         ),
       ),
     );
-    await tester.tap(find.byType(FSwitch));
+    await tester.tap(find.text('Notify'));
     await tester.pumpAndSettle();
     expect(c.values['notify'], 'true');
   });
