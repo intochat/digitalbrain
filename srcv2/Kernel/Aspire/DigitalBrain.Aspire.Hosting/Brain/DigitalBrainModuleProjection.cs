3b95bbc7650ec6def720135dac7837265d3ebacb
Vladyslav Horbachov <0965142838gvs@gmail.com>
2026-08-15 23:25:34 +0200

    Restructuring

--- srcv2/Kernel/Aspire/DigitalBrain.Aspire.Hosting/Brain/DigitalBrainModuleProjection.cs
+++
@@ -1,9 +0,0 @@
-using Aspire.Hosting.ApplicationModel;
-
-namespace DigitalBrain.Aspire.Hosting;
-
-public abstract class DigitalBrainModuleProjection
-{
-    public abstract void Apply<TResource>(IResourceBuilder<TResource> builder)
-        where TResource : IResourceWithEnvironment, IResourceWithEndpoints;
-}
